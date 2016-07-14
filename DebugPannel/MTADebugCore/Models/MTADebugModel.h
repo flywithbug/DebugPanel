8c497bdf367119f64588043457d869e6769531b8
程二枫 <369495368@qq.com>
2016-07-14 13:49:06 +0800

    参数注释

--- DebugPannel/MTADebugCore/Models/MTADebugModel.h
+++ DebugPannel/MTADebugCore/Models/MTADebugModel.h
@@ -8,7 +8,13 @@
 #import <Foundation/Foundation.h>
 
 
-
+/**
+ *  @author Jack, 16-07-14 12:07:12
+ *
+ *  title: 显示名称
+ *  selectorProperty: 调用的方法签名
+ *  etc: 额外参数
+ */
 @interface MTADebugModel : NSObject <NSCoding, NSCopying>
 
 @property (nonatomic, strong) NSString *title;
